64da90cc9d6db78d12878e000f80918557182c01
Poonji <poonjij21@gmail.com>
2023-08-17 23:52:08 +0530

    commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import './Item.css'
 function Item()
 {
+    const itemName="Nirma";
     return (<p className="nirma">Nirma</p>);
 }
 
--- src/components/ItemDate.js
+++ src/components/ItemDate.js
@@ -2,10 +2,13 @@ import './ItemDate.css';
 
 function ItemDate()
 {
+    const day=10;
+    const month="october";
+    const year=2001;
     return(<div className="Date">
-        <span>21 </span>
-        <span>October </span>
-        <span>2001</span>
+        <span>{day}</span>
+        <span>{month} </span>
+        <span>{year}</span>
     </div>);
 }
 export default ItemDate;
