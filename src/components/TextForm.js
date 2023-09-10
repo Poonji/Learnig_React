bc47a2aa3136b2c5a4a5ab1780d9cd6db96ababc
Poonji <poonjij21@gmail.com>
2023-09-10 15:08:48 +0530

    functions

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,9 +12,10 @@ export default function TextForm(props)
     console.log("on  change");
     setText(event.target.value);
   }
-  const[text,setText]=useState('enter text here');
+  const[text,setText]=useState('');
     return (
-        <div>
+      <>
+        <div className="container">
           <h1>{props.heading}</h1>
   <div className="mb-3">
     <label for="myBox" class="form-label">Enter your text below</label>
@@ -23,5 +24,14 @@ export default function TextForm(props)
    <button className="button.btn.btn-primary" onClick={handleUpClick}>convert to uppercase </button>
  
   </div>
+  <div className="container my-3">
+    <h2>Your text summary here</h2>
+    <p>{text.split(" ").length} words and {text.length} char</p>
+    <p>{0.008*text.split(" ").length}Minutes read</p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+
+  </div>
+      </>
   )
     }
