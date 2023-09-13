b836c32a1eb412bf8d3031353bd97a31e7e44444
Poonji <poonjij21@gmail.com>
2023-09-13 23:54:15 +0530

    enabling dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Item from './components/Item'
-import ItemDate from './components/ItemDate';
-let name="poonji";
+//import TextForm from './components/TextForm';
+//import Item from './components/Item'
+//import ItemDate from './components/ItemDate';
+import About from './components/About';
+//let name="poonji";
 function App() {
   return (
     <>
      <Navbar title="TextUtils" />
      <div className="container my-3">
-     <TextForm heading="enter the text to analyze"/>
+   { /* <TextForm heading="enter the text to analyze"/> */}
+   <About/>
      </div>
 
     </>
